6730a34d5fec571a2fa058e192705bf41fd9b8c8
yu <yulya.yusupova.2020@inbox.ru>
2024-07-06 03:26:42 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const server = http.createServer();
+
+server.on('request', (request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    response.end('<h1>Привет, октагон!</h1>');
+});
+server.listen(3000, () => {"Сервер начал прослушивание запросов на порту 3000"});
