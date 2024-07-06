0ec896a352ef257e3174479e774c1f4820fad4df
yu <yulya.yusupova.2020@inbox.ru>
2024-07-06 23:38:31 +0800

    Доработан сервер согласно заданию

--- app.js
+++ app.js
@@ -1,8 +1,43 @@
-const http = require("http");
-const server = http.createServer();
+const express = require("express");
+const app = express();
+app.use(express.json());
 
-server.on('request', (request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    response.end('<h1>Привет, октагон!</h1>');
+app.get("/static", (req, res) => {
+    const jsonResponse = {
+        header: "Hello",
+        body: "Octagon NodeJS Test"
+    };
+    res.json(jsonResponse);
 });
-server.listen(3000, () => {"Сервер начал прослушивание запросов на порту 3000"});
+
+app.get("/dynamic", (req, res) => {
+    
+    const { a, b, c } = req.query;
+    
+    const numberA = parseFloat(a);
+    const numberB = parseFloat(b);
+    const numberC = parseFloat(c);
+
+    if (isNaN(numberA) || isNaN(numberB) || isNaN(numberC)) {
+        
+        const jsonResponse = {
+            header: "Error"
+        };
+        res.json(jsonResponse);
+    } 
+    
+    else {
+  
+        const result = (numberA * numberB * numberC) / 3;
+        const jsonResponse = {
+            header: "Calculated",
+            body: result.toString()
+        };
+        res.json(jsonResponse);
+    }
+});
+
+
+app.listen(3000, () => {
+    console.log(`Сервер начал прослушивание запросов на порту 3000`);
+});
